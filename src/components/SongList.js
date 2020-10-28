376f8bf6630d567df4457589b62160907f1bea97
Bartlomiej Szala <bs@vedevo.io>
2020-10-28 12:50:09 +0100

    wip

---
+++ src/components/SongList.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectSong} from "../actions";
+
+class SongList extends Component{
+
+    renderList(){
+        return this.props.songs.map(s => {
+           return (
+               <div className="item" key={s.title}>
+                   <div className="right floated content">
+                       <button
+                           className="ui button primary"
+                           onClick={() => this.props.selectSong(s)}
+                       >
+                           Select
+                       </button>
+                   </div>
+                   <div className="content">
+                       {s.title}
+                   </div>
+               </div>
+           );
+        });
+    }
+
+    render() {
+        return <div className="ui divided list">{this.renderList()}</div>;
+    }
+}
+
+const mapStateToProps = state => {
+    return {songs: state.songs};
+};
+
+export default connect(mapStateToProps,{
+    selectSong: selectSong
+})(SongList);
